11304ef9ba38026e6226ec5e57232b49d2fb8f41
Jeremy Mion <jeremy.mion@yahoo.ca>
2019-04-03 21:25:38 +0200

    part 3.2 start

--- partie3/src/Environment/OrganicEntity.hpp
+++ partie3/src/Environment/OrganicEntity.hpp
@@ -8,6 +8,10 @@
 #include <Obstacle/CircularCollider.hpp>
 #include <Interface/Drawable.hpp>
 #include <Interface/Updatable.hpp>
+class Scorpion;
+class Gerbil;
+class Food;
+
 
 class OrganicEntity : public CircularCollider, public Drawable, public Updatable{
 public:
@@ -19,6 +23,31 @@ public:
      */
     OrganicEntity(const Vec2d& position, double size, double energyLevel);
 
+
+    /*!
+     * Determins if entity is eatable.
+     *
+     * To achive this this methode uses double dispatche. It will call the methode eatableBy.
+     * This allows the management of how can eat each animal up to itself. this is usefull
+     * because it gives the management to the animal itself
+     * @param entity that we want to know if this can eat
+     * @return true if it is eatable
+     */
+    virtual bool eatable(OrganicEntity const* entity) const = 0;
+
+    /*!
+     * Determins if this can be eaten by a scorpion.
+     */
+    virtual bool eatableBy(Scorpion  const* scorpion) const = 0;
+    /*!
+     * Determins if this can be eaten by a gerbil.
+     */
+    virtual bool eatableBy(Gerbil const* gerbil) const = 0;
+    /*!
+     * Determins if this can be eaten by a food.
+     */
+    virtual bool eatableBy(Food const* food) const = 0;
+protected:
 
 private:
 
